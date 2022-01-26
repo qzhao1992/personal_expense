d2b34ed2ddfa0f9b2b445fa196aa9cd8f4e7700c
agentq_kalidy <qzhao@uco.edu>
2022-01-26 17:58:58 -0600

    adding color of chart_bar

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import "package:intl/intl.dart";
+import 'package:personal_expenses/widgets/chart_bar.dart';
 import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
@@ -19,15 +20,28 @@ class Chart extends StatelessWidget {
       return {'day': DateFormat.E().format(weekDay).substring(0,1), 'amount': totalSum};
     });
   }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item){
+      return sum + (item['amount'] as double);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: [
-
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(data['day'] as String, data['amount'] as double, totalSpending == 0.0 ? 0.0 : (data['amount'] as double) / totalSpending));
+          }).toList(),
+        ),
       ),
     );
   }
