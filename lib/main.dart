819a351389baf3f0fced72ff426592bd6b06f3d9
agentq_kalidy <qzhao@uco.edu>
2022-02-01 17:02:00 -0600

    added orentation

--- lib/main.dart
+++ lib/main.dart
@@ -121,6 +121,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
     final appBar = AppBar(
         title: Text('Personal Expenses'),
         actions: [
@@ -130,6 +131,10 @@ class _MyHomePageState extends State<MyHomePage> {
           )
         ],
       );
+      final txListWidget = Container(
+              height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.7,
+              child: TransactionList(_userTransactions, _deleteTransaction),
+              );
     return Scaffold(
       appBar: appBar,
       body: SingleChildScrollView(
@@ -137,7 +142,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Row(
+            if (isLandscape) Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text('Show Chart'),
@@ -150,13 +155,16 @@ class _MyHomePageState extends State<MyHomePage> {
                   },
                 )
             ],),
-            _showChart ? Container(
+            if(!isLandscape) Container(
+                height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.3,
+                child: Chart(_recentTransactions),
+              ),
+            if(!isLandscape) txListWidget,
+            if(isLandscape) _showChart ? Container(
               height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.7,
               child: Chart(_recentTransactions),
             ) :
-            Container(
-              height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.7,
-              child: TransactionList(_userTransactions, _deleteTransaction)),
+            txListWidget,
           ],
         ),
       ),
