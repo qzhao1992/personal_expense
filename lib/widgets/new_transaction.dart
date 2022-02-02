ecc512bc6b975807615b262385b5b1c0a842688a
agentq_kalidy <qzhao@uco.edu>
2022-02-02 14:59:21 -0600

    fixing keyboard

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -53,48 +53,55 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override 
   Widget build(BuildContext context){
-    return Card(
-            elevation: 5,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Title'),
-                    controller: _titleController,
-                    onSubmitted: (_) => _submitData(),
+    return SingleChildScrollView(
+      child: Card(
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.only(
+                  top: 10,
+                  left: 10,
+                  right: 10,
+                  bottom: MediaQuery.of(context).viewInsets.bottom + 10
                   ),
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Amount'),
-                    controller: _amountController,
-                    keyboardType: TextInputType.number,
-                    onSubmitted: (_) => _submitData(),
-                  ),
-                  Container(
-                    height: 70,
-                    child: Row(children: [
-                      Expanded(child: Text('Picked Date: ${DateFormat.yMd().format(_selectedDate)}')),
-                      FlatButton(
-                        textColor: Theme.of(context).primaryColor,
-                        child: Text(
-                          'Choose Date',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      controller: _titleController,
+                      onSubmitted: (_) => _submitData(),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Amount'),
+                      controller: _amountController,
+                      keyboardType: TextInputType.number,
+                      onSubmitted: (_) => _submitData(),
+                    ),
+                    Container(
+                      height: 70,
+                      child: Row(children: [
+                        Expanded(child: Text('Picked Date: ${DateFormat.yMd().format(_selectedDate)}')),
+                        FlatButton(
+                          textColor: Theme.of(context).primaryColor,
+                          child: Text(
+                            'Choose Date',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
+                          onPressed: _presentDatePicker,
                         ),
-                        onPressed: _presentDatePicker,
-                      ),
-                    ],),
-                  ),
-                  RaisedButton(
-                    onPressed: _submitData,
-                    child: Text('Add Transaction'),
-                    color: Theme.of(context).primaryColor,
-                    textColor: Colors.white,
-                  ),
-                ],
+                      ],),
+                    ),
+                    RaisedButton(
+                      onPressed: _submitData,
+                      child: Text('Add Transaction'),
+                      color: Theme.of(context).primaryColor,
+                      textColor: Colors.white,
+                    ),
+                  ],
+                ),
               ),
             ),
-          );
+    );
   }
 }
