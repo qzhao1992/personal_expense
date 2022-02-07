cb084ec6a19712d74ba81df4036142260ec35bd3
agentq_kalidy <qzhao@uco.edu>
2022-02-07 17:02:33 -0600

    refactory code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:personal_expenses/widgets/chart.dart';
 import 'package:personal_expenses/widgets/new_transaction.dart';
@@ -130,7 +131,18 @@ class _MyHomePageState extends State<MyHomePage> {
     final mediaQuery = MediaQuery.of(context);
     final isLandscape = mediaQuery.orientation == Orientation.landscape;
 
-    final appBar = AppBar(
+    final dynamic appBar = Platform.isIOS 
+    ? CupertinoNavigationBar(
+      middle: Text('Personal Expenses',),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+        GestureDetector(
+          child: Icon(Icons.add),
+          onTap: () => _startAddNewTransaction(context),)
+      ],),
+    ) 
+    : AppBar(
       title: Text('Personal Expenses'),
       actions: [
         IconButton(
@@ -139,6 +151,8 @@ class _MyHomePageState extends State<MyHomePage> {
         )
       ],
     );
+
+    
     final txListWidget = Container(
       height: (mediaQuery.size.height -
               appBar.preferredSize.height -
@@ -146,9 +160,8 @@ class _MyHomePageState extends State<MyHomePage> {
           0.7,
       child: TransactionList(_userTransactions, _deleteTransaction),
     );
-    return Scaffold(
-      appBar: appBar,
-      body: SingleChildScrollView(
+
+    final pageBody = SafeArea(child: SingleChildScrollView(
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -157,7 +170,7 @@ class _MyHomePageState extends State<MyHomePage> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text('Show Chart'),
+                  Text('Show Chart', style: Theme.of(context).textTheme.headline6,),
                   Switch.adaptive(
                     activeColor: Colors.purple,
                     value: _showChart,
@@ -190,7 +203,11 @@ class _MyHomePageState extends State<MyHomePage> {
                   : txListWidget,
           ],
         ),
-      ),
+      ),);
+
+    return Platform.isIOS ? CupertinoPageScaffold(child: pageBody, navigationBar: appBar,) : Scaffold(
+      appBar: appBar,
+      body: pageBody,
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: Platform.isIOS
           ? Container()
--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -61,7 +61,7 @@ class _NewTransactionState extends State<NewTransaction> {
                   top: 10,
                   left: 10,
                   right: 10,
-                  bottom: MediaQuery.of(context).viewInsets.bottom + 10
+                  bottom: MediaQuery.of(context).viewInsets.bottom + 10,
                   ),
                 child: Column(
                   children: [
